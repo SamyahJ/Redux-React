297e44bba281fb2ea3f0878c59315ee8337a4e90
SamyahJ <54041272+SamyahJ@users.noreply.github.com>
2021-01-14 20:53:27 -0500

    Create index.js

---
+++ src/store/index.js
@@ -0,0 +1,26 @@
+import { createStore } from 'redux';
+import devToolsEnhancer from 'remote-redux-devtools';
+
+
+const initialState = {
+  posts: [{id: 1, title: 'Test Post'}],
+  loginModal: {
+    open: false
+  }
+}
+
+const reducer = (state = initialState, action) => {
+  if (action.type === 'ADD_POST') {
+    return Object.assign({}, state, {
+      posts: state.posts.concat(action.payload)
+    })
+  }
+
+  return state
+}
+
+
+const store = createStore(reducer, devToolsEnhancer(window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__)
+);
+
+export default store
